67592ca11ff75eaaf5314816baf11925e831f982
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-05 01:49:40 +0700

    Update boostStore.ts

--- src/store/boostStore.ts
+++ src/store/boostStore.ts
@@ -1,23 +1,16 @@
+// src/store/boostStore.ts
 import { create } from "zustand"
 
-interface BoostState {
-  activeBoost: "none" | "double" | "regen"
-  boostEndTime: number | null
-  activateBoost: (type: "double" | "regen", duration: number) => void
-  clearBoost: () => void
+export type BoostType = "doublePoints" | "energyRegen" | null
+
+interface BoostStore {
+  boost: BoostType
+  setBoost: (type: BoostType) => void
+  resetBoost: () => void
 }
 
-export const useBoostStore = create<BoostState>((set) => ({
-  activeBoost: "none",
-  boostEndTime: null,
-  activateBoost: (type, duration) => {
-    const endTime = Date.now() + duration * 1000
-    set({ activeBoost: type, boostEndTime: endTime })
-
-    // auto clear after duration
-    setTimeout(() => {
-      set({ activeBoost: "none", boostEndTime: null })
-    }, duration * 1000)
-  },
-  clearBoost: () => set({ activeBoost: "none", boostEndTime: null }),
+export const useBoostStore = create<BoostStore>((set) => ({
+  boost: null,
+  setBoost: (type) => set({ boost: type }),
+  resetBoost: () => set({ boost: null }),
 }))
