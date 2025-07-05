dd1f2c210bddf2c494dd4a15625b50f0799633b6
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-05 16:33:25 +0700

    Update boostStore.ts

--- src/store/boostStore.ts
+++ src/store/boostStore.ts
@@ -1,30 +1,17 @@
 import { create } from "zustand"
 
 interface BoostState {
-  doublePointActive: boolean
-  energyRegenActive: boolean
-  doublePointDuration: number
-  energyRegenDuration: number
-  activateBoost: (type: "double" | "regen", durationInSeconds: number) => void
+  activeBoost: "none" | "double" | "energy"
+  activateBoost: (type: "double" | "energy", duration: number) => void
 }
 
 export const useBoostStore = create<BoostState>((set) => ({
-  doublePointActive: false,
-  energyRegenActive: false,
-  doublePointDuration: 0,
-  energyRegenDuration: 0,
-
+  activeBoost: "none",
   activateBoost: (type, duration) => {
-    if (type === "double") {
-      set({ doublePointActive: true, doublePointDuration: duration })
-      setTimeout(() => {
-        set({ doublePointActive: false, doublePointDuration: 0 })
-      }, duration * 1000)
-    } else if (type === "regen") {
-      set({ energyRegenActive: true, energyRegenDuration: duration })
-      setTimeout(() => {
-        set({ energyRegenActive: false, energyRegenDuration: 0 })
-      }, duration * 1000)
-    }
-  }
+    set({ activeBoost: type })
+
+    setTimeout(() => {
+      set({ activeBoost: "none" })
+    }, duration * 1000)
+  },
 }))
